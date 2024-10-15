ce07331359e724212fbe0867e1495e51a951d132
Sophie Greenwood <sjwood1515@gmail.com>
2024-10-15 04:50:42 -0400

    Clean up code

--- delegation_utils.py
+++ delegation_utils.py
@@ -133,3 +133,47 @@ def find_delegate_iteratively(h,m,s,d,f, quiet=True):
             improving = False
 
     return best_model, best_loss, Y, human_errors, count
+
+    def find_Rk(u, k):
+    min_var = np.Inf
+    best_t = None
+    for t in range(len(u)-k+1):
+        u_R = u[t:t+k]
+        variance = np.var(u_R)
+        if variance < min_var:
+            min_var = variance
+            best_t = t
+    return best_t, min_var
+
+def find_best_model_separable_alg(u_unsrt,w,h):
+    u_order = np.argsort(u_unsrt)
+    u = np.array(u_unsrt)[u_order]
+    
+    H = len(u)
+
+    var_w = np.var(w)
+
+    best_loss = np.Inf
+    best_k = None
+    best_tk = None
+    for k in range(H+1):
+        if k == 0:
+            loss = var_w
+            best_t = None
+        else:
+            best_t, min_var_uk = find_Rk(u, k)
+            loss = (1-(k/H))*var_w + (k/H)*min_var_uk
+
+        if loss < best_loss:
+            best_loss = loss
+            best_tk = best_t
+            best_k = k
+
+    if best_k == 0:
+        R = []
+    else:
+        R = np.sort(np.arange(H)[u_order][best_tk:best_tk+best_k])
+
+    Y = [Y for i, Y in enumerate(itertools.product(range(2),repeat=h)) if i not in R]
+
+    return Y
